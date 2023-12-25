172a7dd16a762e89d9e499644c46b675fec6ff28
Yossi4 <154239866+Yossi4@users.noreply.github.com>
2023-12-25 17:42:52 +0200

    first test

--- Divisors.java
+++ Divisors.java
@@ -1,8 +1,26 @@
-/**
- *  Gets a command-line argument (int), and prints all the divisors of the given number.
- */
 public class Divisors {
 	public static void main (String[] args) {
-		//// Put your code here
+		int x = Integer.parseInt(args[0]);
+		int potentialDevisor = 1;
+
+		while (potentialDevisor < x+1)
+		{
+			if (x % potentialDevisor == 0){
+
+				System.out.println(potentialDevisor);
+			}
+				
+
+			potentialDevisor ++;
+
+
+		}
+
+
+
+
+
+
+
 	}
 }
