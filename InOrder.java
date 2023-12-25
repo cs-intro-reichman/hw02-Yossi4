4674f829d8b34c8546c427ec4107a8153c76467f
Yossi4 <154239866+Yossi4@users.noreply.github.com>
2023-12-25 19:48:58 +0200

    first test

--- InOrder.java
+++ InOrder.java
@@ -1,9 +1,30 @@
-/**
- *  Generates and prints random integers in the range [0,10),
- *  as long as they form a non-decreasing sequence.
- */
+import java.util.*;
 public class InOrder {
 	public static void main (String[] args) {
-		//// Write your code here
+		
+		int last = -1;
+		int current = (int) (Math.random() * 10);
+
+		
+
+		while (current>last){
+				System.out.print(current);
+				System.out.print(" ");
+				last = current;
+				current = (int) (Math.random() * 10);
+
+
+		}
+
+
+
+
+			
+		
+
+
+
+
+
 	}
 }
