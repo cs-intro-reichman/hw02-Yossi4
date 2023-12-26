53e868106ea0e2806f40b0eb8820acf90355671e
Yossi4 <154239866+Yossi4@users.noreply.github.com>
2023-12-26 09:30:22 +0200

    first test

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -1,8 +1,28 @@
-/**
- *  Gets a command-line argument n (int), and prints an n-by-n damka board.
- */
 public class DamkaBoard {
 	public static void main(String[] args) {
-		//// Put your code here
+		
+		int n = Integer.parseInt(args[0]);
+
+		for (int i = 1; i <= n ;i++ ) {
+			for (int j = 1; j <= n  ; j++ ) {
+
+				System.out.print("* ");
+				
+			}
+				if (i % 2 != 0) {
+					System.out.println("");
+					System.out.print(" ");
+
+				}
+				else{
+
+
+					System.out.println("");
+				}
+
+
+		}
+
+
 	}
 }
