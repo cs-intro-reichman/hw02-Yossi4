b9dab99f0a2376a3d99428ae59cbe0ac6d6b6338
Yossi4 <154239866+Yossi4@users.noreply.github.com>
2023-12-28 07:46:54 +0200

    optimized for and if statments

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -1,25 +1,31 @@
 public class DamkaBoard {
 	public static void main(String[] args) {
-		
-		int n = Integer.parseInt(args[0]);
+				int n = Integer.parseInt(args[0]);
 
 		for (int i = 1; i <= n ;i++ ) {
-			for (int j = 1; j <= n  ; j++ ) {
 
-				System.out.print("* ");
-				
-				
+
+						if (i % 2 == 0) {
+				for (int j = 1; j <= n  ; j++ ) {
+					System.out.print(" *");
+					
+				}
+				System.out.println("");
 			}
-				if (i % 2 != 0) {
-					System.out.println("");
-					System.out.print(" ");
+			else{
+					for (int j = 1; j <= n  ; j++ ) {
+						System.out.print("* ");
+
 
 				}
-				else{
+				System.out.println("");
 
-					System.out.println("");
-				}
-				//
+
+
+		}
+
+
+
 
 		}
 
